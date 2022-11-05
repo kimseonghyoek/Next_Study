9da986365a72bddd756926a5865032b28d0d8877
Kimseonghyoek <cilo1223@naver.com>
2022-11-06 05:17:13 +0900

    Study next style component

--- pages/users/[name].jsx
+++ pages/users/[name].jsx
@@ -1,9 +1,30 @@
 import {} from 'isomorphic-unfetch';
 import React from 'react';
+import css from 'styled-jsx/css';
+
+const style = css`
+  h2 {
+    margin-left: 20px;
+  }
+  .user-bio {
+    margin-top: 12px;
+    font-style: italic;
+  }
+`;
 
 const name = ({ user }) => {
   const username = user && user.name;
-  return <div>{username}</div>
+  return <>
+    {user ? (
+      <div>
+        <h2>{user.name}</h2>
+        <p className='user-bio'>{user.bio}</p>
+      </div>
+    ) : (
+      <div>유저 정보가 없습니다.</div>
+    )}
+    <style jsx>{style}</style>
+  </>
 };
 
 name.getInitialProps = async ({ query }) => {
