bd46a377fbe4ba531c4bc5f7bee9287efb95f359
Kimseonghyoek <cilo1223@naver.com>
2022-11-12 03:58:12 +0900

    Update Styled Component

--- pages/users/[name].jsx
+++ pages/users/[name].jsx
@@ -1,58 +1,17 @@
 import {} from 'isomorphic-unfetch';
 import React from 'react';
-import css from 'styled-jsx/css';
-
-const style = css`
-  .profile-box {
-    width: 25%;
-    max-width: 272px;
-    margin-right: 26px;
-  }
-  .profile-image-wrapper {
-    width: 100%;
-    border: 1px solid #ele4e8;
-  }
-  .profile-image-wrapper .profile-image {
-    display: block;
-    width: 100%;
-  }
-  .profile-username {
-    margin: 0;
-    padding-top: 16px;
-    font-size: 26px;
-  }
-  .profile-user-login {
-    margin: 0;
-    font-size: 20px;
-  }
-  .profile-user-bio {
-    margin : 0;
-    padding-top: 16px;
-    font-size: 14px;
-  }
-`;
+import Profile from '../../components/Profile';
 
 const name = ({ user }) => {
   if (!user) {
     return null;
   }
-  return <>
-    <div className='profile-box'>
-      <div className='profile-image-wrapper'>
-        <img
-          className='profile-image'
-          src={user.avatar_url}
-          alt={`${user.name} 프로필 이미지`}
-        />
-      </div>
-      <h2 className='profile-username'>{user.name}</h2>
-      <p className='profile-user-login'>{user.login}</p>
-      <p className='profile-user-bio'>{user.bio}</p>
-    </div>
-    <style jsx>{style}</style>
-  </>
-};
-
+  return (
+    <>
+      <Profile user={user} />
+    </>
+  )
+}
 name.getInitialProps = async ({ query }) => {
   const { name } = query;
   try {
